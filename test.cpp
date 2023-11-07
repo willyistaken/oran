09ec71f9cdecb354928e05712f75ac93153d4a72
ancuber1031 <ancuber1031@pop-os.localdomain>
2023-11-07 15:58:06 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#pragma GCC optimize("Ofast","unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int,int>
+#define p_q priority_queue
+#define endl '\n'
+#define pb push_back
+
+signed main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    cout<<"Hello World!\n";
+    return 0;
+}
