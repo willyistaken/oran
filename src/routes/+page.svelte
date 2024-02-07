4df3ddf53189fddbfa7ddd467fa115d51ab5dc60
willyistaken <willyhlchan@gmail.com>
2024-02-07 17:38:27 +0800

    tooltip hover 2/7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script>
   import { base } from '$app/paths';
 </script>
-<center><h1>賴泓安破台</h1>
+<center><h1>賴泓安<span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span></h1>
 <h2>接下來我不太確定要怎麼做</h2>
 <img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
 <button class="demo" >做專題</button>
 </center>
-
-
