958b71ce97a05cfa02f93d9530cd291da3924b34
willyistaken <willyhlchan@gmail.com>
2024-02-11 13:54:10 +0800

    svelte d3 interective

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
   import { base } from '$app/paths';
+  import Test from './interective_test.svelte';
 </script>
 <center><h1>賴泓安<span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span></h1>
 <h2>接下來我不太確定要怎麼做</h2>
 <img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
 <button class="demo" >做專題</button>
+<Test/>
 </center>
