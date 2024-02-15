392ea53bf4d489067f38efd2863717dc898fe242
ancuber1031 <ancuber1031@pop-os.localdomain>
2024-02-15 11:42:05 +0800

    2/5 lottie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,6 @@
 <h2>接下來我不太確定要怎麼做</h2>
 <img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
 <button class="demo" >做專題</button>
-<Test/>
+<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+<dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
 </center>
