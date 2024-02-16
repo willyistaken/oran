c2911dfef028fcb5d91867765dbdaff50d24c110
ANcuber <ansona1131@gmail.com>
2024-02-16 21:09:49 +0800

    2/16 adding some contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,18 @@
 <body>
 <center>
   <h1>Website Title</h1>
-    <h2>8th YTP</h2>
-    <img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
+    <h2>8th YTP 壹伍伍捌</h2>
+    <img src='https://i.imgur.com/SgCSz5F.png' alt="young turing program" width="1500" height="200"/>
 </center>
 
 <h1>Purpose of this website</h1>
   <p>
     In the era of 5G technology, computers have become an integral part of human life. We chat with others, watch various videos, or even purchase items online. People do almost anything on the internet! Consequently, it is crucial for us to understand how the digital world works. To this end, we have created this website to provide an overview of O-RAN and PTP (Precision Time Protocol), both of which are related to 5G technology.
   </p>
+<br>
 
 <center><h1>O-RAN</h1></center>
+
 <h2>What exactly is RAN?</h2>
   <p>
     RAN or Radio Access Network is one of the major component of the mobile data network. It is responsible for connecting User Equipments (UE) with the Central Network (CN) wirelessly.
@@ -44,14 +46,27 @@
   The basic structure of a mobile network is that of cellular network, where land areas are divided into cells and is served by one or more base station.
 </p>
 <!--- pic -->
+<br>
 
 <h2>Okay so like… what is 5G?</h2>
+<!-- meme -->
+<p>
+  The fifth-generation technology standard for cellular networks, commonly referred to as 5G, represents the latest advancement in telecommunications. Deployments of 5G began in 2019, heralding a new era with its promise of high speed and low latency. But what makes 5G so important? What's all the buzz about? To answer these questions, we need to take a detour and delve into the history of RAN (Radio Access Network).
+</p>
+<br>
 
+<h3>The history of RAN</h3>
+<p></p>
+
+
+
+</body>
+<!--
 <center>
   <button class="demo" >做專題</button>
   <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
   <dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
 </center>
-</body>
+-->
 
 <!-- <span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span>*/ > -->
