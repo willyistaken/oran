348833052a43327104584959b736cd1ef80dd381
ANcuber <ansona1131@gmail.com>
2024-02-16 20:50:15 +0800

    2/16 adding some contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import Test from './interective_test.svelte';
 </script>
 
+<body>
 <center>
   <h1>Website Title</h1>
     <h2>8th YTP</h2>
@@ -11,15 +12,46 @@
 
 <h1>Purpose of this website</h1>
   <p>
-    In the era of 5G technology, computers have become an integral part of humans' life. We chat with others, watch various videos or even purchase items online. People do almost anything on the Internet! That is, it is crucial for us to understand how the digital world works. To this end, we create this website to give an overview of O-RAN and PTP(Precision Time Protocol), which is related to 5G technology.
+    In the era of 5G technology, computers have become an integral part of human life. We chat with others, watch various videos, or even purchase items online. People do almost anything on the internet! Consequently, it is crucial for us to understand how the digital world works. To this end, we have created this website to provide an overview of O-RAN and PTP (Precision Time Protocol), both of which are related to 5G technology.
   </p>
 
-<h1>What exactly is RAN?</h1>
+<center><h1>O-RAN</h1></center>
+<h2>What exactly is RAN?</h2>
+  <p>
+    RAN or Radio Access Network is one of the major component of the mobile data network. It is responsible for connecting User Equipments (UE) with the Central Network (CN) wirelessly.
+  </p>
+  <br>
+
+<!-- pic -->
+<h5>RAN can be devided into 2 different parts:</h5>
+<br>
+<dl>
+  <dt>Baseband Unit (BBU):</dt>
+  <dd><blockquote>
+    In the era of 5G technology, computers have become an integral part of human life. We chat with others, watch various videos, or even purchase items online. People do almost anything on the internet! Consequently, it is crucial for us to understand how the digital world works. To this end, we have created this website to provide an overview of O-RAN and PTP (Precision Time Protocol), both of which are related to 5G technology.
+  </blockquote></dd>
+  <dt>Remote Radio Unit (RRU):</dt>
+  <dd><blockquote>
+    In the era of 5G technology, computers have become an integral part of human life. We chat with others, watch various videos, or even purchase items online. People do almost anything on the internet! Consequently, it is crucial for us to understand how the digital world works. To this end, we have created this website to provide an overview of O-RAN and PTP (Precision Time Protocol), both of which are related to 5G technology.
+  </blockquote></dd>
+</dl>
+<br>
+<p>
+  These 2 components communicate with each other via optical fiber or simply a electrical wire. The interface of such connection is called CPRI (Common Public Radio Interface). You can think of it as the USB port on various devices.
+</p>
+<!--- interactive -->
+<p>
+  The basic structure of a mobile network is that of cellular network, where land areas are divided into cells and is served by one or more base station.
+</p>
+<!--- pic -->
+
+<h2>Okay so like… what is 5G?</h2>
 
 <center>
   <button class="demo" >做專題</button>
   <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
   <dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
 </center>
+</body>
 
 <!-- <span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span>*/ > -->
