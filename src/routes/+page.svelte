352688f42217bda6c1afa64c372d808597a09442
ANcuber <ansona1131@gmail.com>
2024-02-16 19:35:53 +0800

    2/16 adding some contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,4 @@
 <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
 <dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
 </center>
+ccc
