f59e677fbe944dc62781a6541df90b109d68625f
ANcuber <ansona1131@gmail.com>
2024-02-16 19:36:21 +0800

    2/16 adding some contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,24 @@
   import { base } from '$app/paths';
   import Test from './interective_test.svelte';
 </script>
-<center><h1>音龍宛在<span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span></h1>
-<h2>接下來我不太確定要怎麼做</h2>
-<img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
-<button class="demo" >做專題</button>
-<script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
-<dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
+
+<center>
+  <h1>Website Title</h1>
+    <h2>8th YTP</h2>
+    <img src='https://avatars.worldcubeassociation.org/uploads/user/avatar/2018LAIH01/1687415701.jpeg' alt="ancuber breaks table" width="300" height="200"/>
 </center>
-ccc
+
+<h1>Purpose of this website</h1>
+  <p>
+    In the era of 5G technology, computers have become an integral part of humans' life. We chat with others, watch various videos or even purchase items online. People do almost anything on the Internet! That is, it is crucial for us to understand how the digital world works. To this end, we create this website to give an overview of O-RAN and PTP(Precision Time Protocol), which is related to 5G technology.
+  </p>
+
+<h1>What exactly is RAN?</h1>
+
+<center>
+  <button class="demo" >做專題</button>
+  <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+  <dotlottie-player src="https://lottie.host/13bb705e-8bed-49f7-90f5-aa29cff7e8b3/JvKCOYlHpu.json" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay class="anime"></dotlottie-player>
+</center>
+
+<!-- <span class="tooltip" data-tooltip="完成所有有可能完成的任務或問題">破台</span>*/ > -->
