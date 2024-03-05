092a906a129cbb33baae6742584cb500ee01ced6
ANcuber <ansona1131@gmail.com>
2024-03-05 14:39:20 +0800

    add interactive

--- src/routes/attacks.svelte
+++ src/routes/attacks.svelte
@@ -1,4 +1,16 @@
+<head>
+    <script src="https://unpkg.com/@lottiefiles/lottie-interactivity@latest/dist/lottie-interactivity.min.js"></script>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+
 <script>
 import { onMount } from 'svelte';
-    
+    onMount(() => {
+        
+    });
 </script>
+
+<body>
+    {#if LottiePlayer}
+    
+</body>
